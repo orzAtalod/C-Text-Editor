6a019c825f650808d98915d148072e4b47e4a6e2
orzAtalod <1759978497@qq.com>
2024-05-21 22:58:32 +0800

    文件结构第二次更新
    补全了textStructure.c，并且给相应的.h添加了简单文档

--- source/textStructure.c
+++ source/textStructure.c
@@ -2,18 +2,29 @@
 #include "fileSystemCore.h"
 #include <stdlib.h>
 
-static int  readBufferInt[255];
-static char readBufferChar[65536];
+static int bufferInt[255];
 
 StyleString* ReadStyleString(FILE* f)
 {
 	StyleString* res = (StyleString*)malloc(sizeof(StyleString));
-	fread(readBufferInt,sizeof(int),5,f);
-	res->pointSize = readBufferInt[0];
-	res->indent    = readBufferInt[1];
-	res->color     = LookupIDInColorTable(readBufferInt[2]);
-	res->font      = LookupIDInFontTable(readBufferInt[3]);
-	res->context   = (StyleChar*)malloc(readBufferInt[4]*sizeof(StyleChar));
-	
-	//TODO: 完成StyleChar的读入函数 
+	fread(readBufferInt, sizeof(int), 5, f);
+	res->pointSize = bufferInt[0];
+	res->indent    = bufferInt[1];
+	res->color     = LookupIDInColorTable(bufferInt[2]);
+	res->font      = LookupIDInFontTable(bufferInt[3]);
+	res->contextLen = res->contextSpace = bufferInt[4];
+	res->context   = (StyleChar*)malloc(res->contextSpace*sizeof(StyleChar));
+	fread(res->context, sizeof(StyleChar), res->contextLen, f);
+}
+
+
+void WriteStyleString(StyleString* str, FILE* f)
+{
+	bufferInt[0] = str->pointSize;
+	bufferInt[1] = str->indent;
+	bufferInt[2] = LookupColorNameInColorTable(str->color);
+	bufferInt[3] = LookupFontNameInFontTable(str->font);
+	bufferInt[4] = str->contextLen;
+	fwrite(bufferInt, sizeof(int), 5, f);
+	fwrite(str->context, sizeof(StyleChar), str->contextLen, f);
 }
--- source/textStructure.h
+++ source/textStructure.h
@@ -1,6 +1,18 @@
 #ifndef __TEXT_STRUCTURE_INCLUDED__
 #define __TEXT_STRUCTURE_INCLUDED__
 
+/*
+*
+* document: 24/5/21 林景行 
+* program: 24/5/21 林景行 
+* last change: 24/5/21 林景行 
+* details in github history
+*
+* 定义结构体StyleChar和StyleString，用于文件操作、编辑逻辑和显示逻辑。
+* 现支持读写 
+*
+*/
+
 typedef struct{
 	char context;
 	char style; //0 for nothing, 1 for Bold, 2 for Italic, 3 for Bold+Italic
@@ -9,12 +21,14 @@ typedef struct{
 typedef struct{
 	int pointSize;
 	int indent;
+	int contextLen;
+	int contextSpace;
 	const char* color;
 	const char* font;
 	StyleChar* context;
 } StyleString;
 
 StyleString* ReadStyleString(FILE* f);
-StyleString* WriteStyleString(FILE* f);
+void         WriteStyleString(StyleString* str, FILE* f);
 
 #endif
