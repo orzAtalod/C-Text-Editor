1cff541bed46768fd0f45435b1d07aa5ce8b958f
orzAtalod <1759978497@qq.com>
2024-05-22 11:10:14 +0800

    完成fileSystem最终集成
    在fileSystem.c中完成了依赖注入步骤，定义了文件整体结构，并且提供了从文件名开始直接完成存读的过程

---
+++ source/fileSystem.c
@@ -0,0 +1,68 @@
+#include "fileSystem.h"
+#include <stdio.h>
+#include "blockList.h"
+#include "textStructure.h"
+#include "fileSystemCore.h"
+
+static void* styleStringReader(FILE* f)
+{
+	return (void*)ReadStyleString(f);
+}
+
+static void* styleStringWriter(void* str, FILE* f)
+{
+	WriteStyleString((StyleString*)str, f); 
+}
+
+static int curPage;
+
+void ChangePageAtFileSystem(int page)
+{
+	curPage = page;
+}
+
+static ColorDefinitionFunction colorFunc;
+
+void InitFileSystem(ColorDefinitionFunction func)
+{
+	colorFunc = func;
+	RegisterReaderMethod(1,styleStringReader);
+	RegisterWriterMethod(1,styleStringWriter);
+}
+
+void UpdateColorDefinitionMethod(ColorDifinitionFunction func)
+{
+	colorFunc = func;
+}
+
+void LoadFileAtPage(int page, const char* fileName)
+{
+	FILE* f = fopen(fileName,"rb");
+	ChangePageOfColorTable(page);
+	ReadColorTable(f, colorFunc);
+	ChangePageOfFontTable(page);
+	ReadFontTable(f);
+	LoadBlockList(f);
+	fclose(f);
+}
+
+void SaveFileAtPage(int page, const char* fileName)
+{
+	FILE* f = fopen(fileName,"wb");
+	ChangePageOfColorTable(page);
+	WriteColorTable(f);
+	ChangePageOfFontTable(page);
+	WriteFontTable(f);
+	SaveBlockList(f);
+	fclose(f);
+}
+
+void LoadFile(const char* fileName)
+{
+	LoadFileAtPage(curPage, fileName);
+}
+
+void SaveFile(const char* fileName)
+{
+	SaveFileAtPage(curPage, fileName);
+}
--- source/fileSystem.h
+++ source/fileSystem.h
@@ -1,4 +1,12 @@
 #ifndef __FILE_SYSTEM_H_INCLUDED__
 #define __FILE_SYSTEM_H_INCLUDED__
 
+void InitFileSystem(ColorDefinitionFunction func);
+void UpdateColorDefinitionMethod(ColorDifinitionFunction func);
+void ChangePageAtFileSystem(int page);
+void LoadFileAtPage(int page, const char* fileName);
+void SaveFileAtPage(int page, const char* fileName);
+void LoadFile(const char* fileName);
+void SaveFile(const char* fileName);
+
 #endif
