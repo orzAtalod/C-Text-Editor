7405475c6cd0147dd0e725e586bbf83d68ba9d27
orzAtalod <1759978497@qq.com>
2024-05-27 21:07:09 +0800

    为textStructure.h添加文档

--- header/textStructure.h
+++ header/textStructure.h
@@ -29,7 +29,30 @@ typedef struct{
 	StyleChar* context;
 } StyleString;
 
+/* 
+*
+* Read和Write函数直接与文件操作，直接在文件中进行读写，不应有任何多读和少读 
+*
+*/
 StyleString* ReadStyleString(FILE* f);
-void         WriteStyleString(StyleString* str, FILE* f);
+void WriteStyleString(StyleString* str, FILE* f);
+
+/*
+*
+* GetStyleStringHeight函数通过数据内容指针和宽度直接计算出高度
+* 传入的一切单位都是英寸 
+*
+*/
+void GetStyleStringHeight(StyleString* str, double width);
+
+/*
+*
+* Draw函数通过libgraphics库，直接在(cx, cy)为**左上角**的区域中绘制相应内容
+* 绘制内容宽度为 width，需要绘制高度介于 beginHeight 和 endHeight 之间的部分 
+* 也就是说，绘制内容右下角为(dx+width, dy+endHeight-beginHeight) 
+* 传入的一切单位都是英寸 
+*
+*/
+void DrawStyleString(StyleString* str, double cx, double cy, double width, double beginHeight, double endHeight);
 
 #endif
