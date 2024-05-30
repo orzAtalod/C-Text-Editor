4b60cf37965c93108e04aa6c60389d3b55e87686
orzAtalod <1759978497@qq.com>
2024-05-31 00:32:47 +0800

    添加dictionaryList文件
    作为Explorer的底层数据结构基础添加

---
+++ header/dictionaryList.h
@@ -0,0 +1,96 @@
+#ifndef __LIST_STRUCTURE_H_INCLUDED__
+#define __LIST_STRUCTURE_H_INCLUDED__
+
+//一个用作工具的数据结构文件，维护一个可点击展开的列表 
+//仅定义数据结构和显示方法，不存储任何数据 
+//由于不存储数据，所以不提供遍历方法与操作方法，由下游任务自行实现遍历与操作 
+typedef struct dditemBase{
+	int itemEphasizeType;
+	int itemID;
+	struct dditemBase* prevItem;
+	struct dditemBase* nextItem;
+	struct ddfolderBase* folder;
+	char* itemName; //由 malloc 申请的空间，请保证只有一个指针指向那块内存 
+} DictionaryItem;
+
+typedef struct ddfolderBase{
+	int folderExpended;
+	int folderEmphasizeType; 
+	int folderID;
+	struct ddfolderBase* parent;
+	struct ddfolderBase* prevFolder;
+	struct ddfolderBase* nextFolder;
+	struct dditemBase*   items;
+	struct ddfolderBase* subFolders;
+	char* folderName; //由 malloc 申请的空间，请保证只有一个指针指向那块内存 
+} DictionaryFolder;
+
+typedef struct{
+	int pointSize;
+	int indent;
+	const char* entryFont;   //以下都是外部输入的字符指针，不保证所指向内存的分配方式 
+	const char* folderColors[15];
+	const char* folderBoarderColors[15];
+	const char* folderFillColors[15];
+	const char* itemColors[15];
+} DictionaryGraphicDatas;
+
+/*
+*
+* 在创建的同时设置缺省值 
+* itemEphasizeType 为 0（不强调） 
+* prevItem, nextItem 与 folder 均设置为 NULL
+* 为 itemName 初始设置为 "Noname"
+* 为 itemID 设置一个不与其他初始值重复的初始值（从 1 开始逐个增加） 
+* 
+*/
+
+DictionaryItem* CreateDictionaryItem();
+void FreeDictionaryItem(DictionaryItem*);
+DictionaryItem* CopyDictionaryItem(DictionaryItem* sorce);
+
+/*
+*
+* 在创建的同时设置缺省值 
+* folderExpended 为 0（不展开） 
+* folderEmphasizeType 为 0（不强调） 
+* prevFolder, nextFolder, parent, items, subFolders 均设置为 NULL
+* 为 itemName 初始设置为 "Noname"
+* 为 folderID 设置一个不与其他初始值重复的初始值（从 1 开始逐个增加） 
+* 
+*/
+
+DictionaryFolder* CreateDictionaryFolder();
+void FreeDictionaryFolder(DictionaryFolder*);
+DictionaryFolder* CopyDictionaryFolder(DictionaryFolder* sorce);
+
+/*
+*
+* 在创建的同时会设置缺省值
+* pointSize 为 -1（不更改）
+* indent 为 4 
+* entryFont 为 NULL（不更改）
+* folderColors 为 {"Black","Red","Green","Blue"}
+* folderBoarderColors 为 {NULL, NULL, NULL, NULL} （透明）
+* folderFillColors 为 {NULL, NULL, NULL, NULL} （透明） 
+* itemColors 为 {"Black","Red","Green","Blue"}
+*
+*/
+
+DictionaryGraphicDatas* CreateDictionaryGraphicDatas();
+void FreeDictionaryGraphicDatas(DictionaryFolder*);
+DictionaryGraphicDatas* CopyDictionaryGraphicDatas(DictionaryGraphicDatas* sorce);
+
+void DrawDictionaryList(DictionaryGraphicDatas* dl, DictionaryFolder* fl, double cx, double cy, double width, double begH, double endH);
+double GetDictionaryListHeight(DictionaryGraphicDatas* dl, DictionaryFolder* fl, double width);
+
+typedef struct {
+	int pointEntryType; //0:space 1:folder 2:item
+	DictionaryFolder* folderIn; //NULL if on the outside
+	DictionaryFolder* pointFolder; //NULL if the entry pointed is item or space
+	DictionaryItem*   pointItem; //NULL if the entry pointed is item or space
+} DictionaryCursor;
+
+DictionaryCursor PositionizeDictionaryList(DictionaryGraphicDatas* dl, double width, double mX, double mY);
+
+#endif 
---
+++ source/dictionaryList.c
@@ -0,0 +1,379 @@
+#include "dictionaryList.h"
+
+////////////////////////////////  基础工具  //////////////////////////// 
+
+typedef struct {
+	int indent;
+	int type;
+	void* source;
+	const char* name;
+	const char* penColor;
+	const char* prefix;
+} drawCommand;
+
+static char outputBuffer[255];
+
+static const char* generateLimitedOutput(drawCommand dc, double width)
+{
+	for(int i=0; i<dc.indent; ++i)
+	{
+		outputBuffer[i] = ' ';
+	}
+	strcpy(outputBuffer+dc.indent, dc.prefix);
+	const int offset = dc.indent + strlen(dc.prefix);
+
+	if(!dc.name[0])
+	{
+		if(TextStringWidth(outputBuffer) > width) outputBuffer[0] = '\0';
+		return outputBuffer;
+	}
+
+	outputBuffer[offset] = outputBuffer[outset+1] = '.';
+	outputBuffer[offset+2] = '\0';
+	if(TextStringWidth(outputBuffer) > width)
+	{
+		outputBuffer[0] = '\0';
+		return outputBuffer;
+	}
+
+	int i = 0;
+	for(; dc.name[i]&&TextStringWidth(outputBuffer)<=width; ++i)
+	{
+		outputBuffer[offset+i] = dc.name[i];
+		outputBuffer[offset+i+2] = '.';
+		outputBuffer[offset+i+3] = '\0';
+	}
+	if(TextStringWidth(outputBuffer) > width)
+	{
+		outputBuffer[offset+i+1] = '\0';
+		outputBuffer[offset+i-1] = '.';
+	}
+	else
+	{
+		outputBuffer[offset+i] = '\0';
+	}
+	return outputBuffer;
+}
+
+static drawCommand dicLines[65535];
+static int curline;
+
+static void fillDrawCommandsItem(DictionaryGraphicDatas* dl, DictionaryItem* x, int curindent)
+{
+	if(!x) return;
+	++curline;
+	drawCommand[curline].type = 2;
+	drawCommand[curline].indent = curindent;
+	drawCommand[curline].name = x->itemName;
+	drawCommand[curline].penColor = (dl->itemColors)[x->itemEphasizeType];
+	drawCommand[curline].prefix = "| ";
+	drawCommand[curline].source = (void*)x;
+	fillDrawCommandsItem(dl, x->nextItem, curindent);
+}
+
+static void fillDrawCommandsFolder(DictionaryGraphicDatas* dl, DictionaryFolder* x, int curindent)
+{
+	if(!x) return;
+	++curline;
+	drawCommand[curline].type = 1;
+	drawCommand[curline].indent = curindent;
+	drawCommand[curline].name = x->folderName;
+	drawCommand[curline].penColor = (dl->folderColors)[x->folderEmphasizeType];
+	drawCommand[curline].prefix = x->folderExpended ? "+ " : "- ";
+	drawCommand[curline].source = (void*)x;
+	if(x->folderExpended)
+	{
+		fillDrawCommandsItem(dl, x->items, curindent+dl->indent);
+		fillDrawCommandsFolder(dl, x->subFolders, curindent+dl->indent);
+	}
+	fillDrawCommandsFolder(dl, x->nextFolder, curindent);
+}
+
+static void fillDrawCommands(DictionaryGraphicDatas* dl, DictionaryFolder* x)
+{
+	assert(dl);
+	culine = 0;
+	fillDrawCommandsFolder(dl,x,0);
+}
+
+//////////////////////////////////  绘制与定位逻辑  ////////////////////////
+
+/*
+* pointSize 为 -1（不更改）
+* indent 为 4 
+* entryFont 为 NULL（不更改）
+* folderColors 为 {"Black","Red","Green","Blue"}
+* folderBoarderColors 为 {NULL, NULL, NULL, NULL} （透明）
+* folderFillColors 为 {NULL, NULL, NULL, NULL} （透明） 
+* itemColors 为 {"Black","Red","Green","Blue"}
+*/
+static DictionaryGraphicDatas defaultDicgd;
+static const char* defaultColors[4] = {"Black","Red","Green","Blue"};
+
+static void checkInitialized()
+{
+	if(difaultDicgd.pointSize != -1)
+	{
+		defaultDicgd.pointSize = -1;
+		defaultDicgd.indent = 4;
+		//由于是分配在 static 字段的变量，defaultDicgd 的所有值已被初始化为 0 
+		for(int i=0; i<4; ++i)
+		{
+			defaultDicgd.folderColors[i] = defaultDicgd.itemColors[i] = defaultColors[i];
+		}
+	}
+}
+
+static int pointSizeBuffer;
+static char* fontBuffer;
+static char* penColor;
+
+static void bufferChangeGraphicState(DictionaryGraphicDatas dl)
+{
+	if(!dl) return;
+	penColor = GetPenColor();
+	if(dl->pointSize != -1)
+	{	
+		pointSizeBuffer = GetPointSize();
+		SetPointSize(dl->pointSize);
+	}
+	if(dl->entryFont)
+	{
+		fontBuffer = GetFont();
+		SetFont(dl->entryFont);
+	}
+}
+
+static void retGraphicState(DictionaryGraphicDatas dl)
+{
+	if(!dl) return;
+	SetPenColor(penColor);
+	if(dl->pointSize != -1)
+	{	
+		SetPointSize(pointSizeBuffer);
+	}
+	if(dl->entryFont)
+	{
+		SetFont(fontBuffer);
+	}
+}
+
+double GetDictionaryListHeight(DictionaryGraphicDatas* dl, DictionaryFolder* fl, double width)
+{
+	checkInitialized();
+	if(!dl) dl = &defaultDicgd;
+	bufferChangeGraphicState(dl);
+	
+	fillDrawCommands(dl, fl);
+	const double result = (curline+1)*GetFontHeight();
+
+	retGraphicState(dl);
+	return result;
+}
+
+void DrawDictionaryList(DictionaryGraphicDatas* dl, DictionaryFolder* fl, double cx, double cy, double width, double begH, double endH)
+{
+	checkInitialized();
+	if(!dl) dl = &defaultDicgd;
+	bufferChangeGraphicState(dl);
+	const double cyLimL = cy - begH + endH;
+	cy -= GetFontAscent();
+	
+	const double fHeight = GetFontHeight(); 
+	fillDrawCommands(dl, fl);
+	for(int i=-begH/fHeight+1; i<=curline; ++i)
+	{
+		if(cy<cyLimL) break;
+		const char* outputStr = generateLimitedOutput(dicLines[i], width);
+		MovePen(cx, cy);
+		SetPenColor(dicLines[i].penColor);
+		DrawTextString(outputStr);
+		cy -= fHeight;
+	}
+	
+	retGraphicState(dl);
+}
+
+DictionaryCursor PositionizeDictionaryList(DictionaryGraphicDatas* dl, double width, double mX, double mY)
+{
+	checkInitialized();
+	if(!dl) dl = &defaultDicgd;
+	bufferChangeGraphicState(dl);
+
+	fillDrawCommands(dl,fl);
+	const int resLine = -mY/GetFontHeight();
+	if(resLine<=0 || resLine>curline)
+	{
+		DictionaryCursor res;
+		res.pointEntryType = 0;
+		res.folderIn = res.pointFolder = res.pointItem = 0;
+		return res;
+	}
+	else
+	{
+		DictionaryCursor res;
+		res.pointEntryType = dicLines[resLine].type;
+		res.folderIn = res.pointFolder = res.pointItem = 0;
+		if(res.pointEntryType == 1)
+		{
+			res.pointFolder = (DictionaryFolder*)dicLines[resLine].source;
+			res.folderIn = res.pointFolder;
+		}
+		else
+		{
+			assert(res.pointEntryType == 2);
+			res.pointItem = (DictionaryItem*)dicLines[resLine].source;
+			res.folderIn = res.pointItem.folder;
+		}
+		return res;
+	}
+}
+
+//////////////////////////   创建、拷贝、销毁函数  //////////////////////// 
+#define NEW(x)  ((x*)malloc(sizeof(x)))
+#define VARR(x,len) ((x*)malloc((len)*sizeof(x)))
+
+DictionaryGraphicDatas* CopyDictionaryGraphicDatas(DictionaryGraphicDatas* sorce)
+{
+	DictionaryGraphicDatas* dest = NEW(DictionaryGraphicDatas);
+	memcpy(dest, source, sizeof(DictionaryGraphicDatas));
+	return dest;
+}
+
+DictionaryGraphicDatas* CreateDictionaryGraphicDatas()
+{
+	checkInitialized();
+	return CopyDictionaryGraphicDatas(defaultDicgd);
+}
+
+void FreeDictionaryGraphicDatas(DictionaryGraphicDatas* dg)
+{
+	free(dg);
+}
+
+#define NAME_SIZE 255
+
+/*
+*
+* 在创建的同时设置缺省值 
+* itemEphasizeType 为 0（不强调） 
+* prevItem, nextItem 与 folder 均设置为 NULL
+* itemName 设置为 "Noname"
+* 
+*/
+
+DictionaryItem* CreateDictionaryItem()
+{
+	static int IDs = 0;
+	DictionaryItem* ni = NEW(DictionaryItem);
+	ni->itemID   = ++IDs;
+	ni->itemName = VARR(char, NAME_SIZE);
+	strcpy(ni->itemName, "Noname"); 
+}
+
+void FreeDictionaryItem(DictionaryItem* di)
+{
+	if(di->folder && di->folder->items == di)
+	{
+		di->folder->items = 0;
+		if(di->prevItem) di->folder->items = di->prevItem;
+		if(di->nextItem) di->folder->items = di->nextItem;
+	}
+	if(di->prevItem) di->prevItem->nextItem = di->nextItem;
+	if(di->nextItem) di->nextItem->prevItem = di->prevItem;
+	free(di->itemName);
+	free(di);
+}
+
+DictionaryItem* CopyDictionaryItem(DictionaryItem* sorce)
+{
+	DictionaryItem* dest = CreateDictionaryItem();
+	dest->itemEphasizeType = sorce->itemEphasizeType;
+	dest->itemID = sorce->itemID;
+	dest->folder = sorce->folder;
+	strcpy(dest->itemName, sorce->itemName);
+	return dest;
+}
+
+DictionaryFolder* CreateDictionaryFolder()
+{
+	static int IDs = 0;
+	DictionaryFolder* nf = NEW(DictionaryFolder);
+	nf->folderID   = ++IDs;
+	nf->folderName = VARR(char, NAME_SIZE);
+	strcpy(nf->folderName, "Noname");
+}
+
+void FreeDictionaryFolder(DictionaryFolder* df)
+{
+	if(df->parent && df->parent->subFolders==df)
+	{
+		df->parent->subFolders = 0;
+		if(df->prevFolder) df->parent->subFolders = df->prevFolder;
+		if(df->nextFolder) df->parent->subFolders = df->nextFolder;
+	}
+	if(df->subFolders)
+	{
+		while(df->subFolders) FreeDictionaryFolder(df->subFolders);
+	}
+	if(df->items)
+	{
+		while(df->items) FreeDictionaryItem(df->items);
+	}
+	if(df->prevFolder) df->prevFolder->nextFolder = df->nextFolder;
+	if(df->nextFolder) df->nextFolder->prevFolder = df->prevFolder;
+	free(df->folderName);
+	free(df);
+}
+
+DictionaryFolder* CopyDictionaryFolder(DictionaryFolder* sorce)
+{
+	DictionaryFolder* dest    = CreateDictionaryFolder();
+	dest->folderID            = sorce->folderID;
+	dest->folderExpended      = sorce->folderExpended;
+	dest->folderEmphasizeType = sorce->folderEmphasizeType;
+	dest->parent              = sorce->parent;
+	strcpy(dest->folderName, sorce->folderName);
+	
+	if(sorce->items)
+	{
+		dest->items = CopyDictionaryItem(sorce->items);	
+
+		DictionaryItem* dt = dest->items;
+		for(DictionaryItem* it = sorce->items->nextItem; it; it=it->nextItem)
+		{
+			dt->nextItem = CopyDictionaryItem(it);
+			dt->nextItem->prevItem = dt;
+			dt = dt->nextItem;
+		}
+	
+		dt = dest->items;
+		for(DictionaryItem* it = sorce->items->prevItem; it; it=it->prevItem)
+		{
+			dt->prevItem = CopyDictionaryItem(it);
+			dt->prevItem->nextItem = dt;
+			dt = dt->prevItem;
+		}
+	}
+	
+	if(sorce->subFolders)
+	{
+		dest->subFolders = CopyDictionaryFolder(sorce->subFolders);
+		
+		DictionaryFolder* df = dest->subFolders;
+		for(DictionaryFolder* it = sorce->subFolders->nextFolder; it; it=it->nextFolder)
+		{
+			df->nextFolder = CopyDictionaryFolder(it);
+			df->nextFolder->prevFolder = df;
+			df = df->nextFolder;
+		}
+		
+		df = dest->subFolders;
+		for(DictionaryFolder* it = sorce->subFolders->prevFolder; it; it=it->prevFolder)
+		{
+			df->prevFolder = CopyDictionaryFolder(it);
+			df->prevFolder->nextFolder = df;
+			df = df->nextFolder;
+		}
+	}
+}
